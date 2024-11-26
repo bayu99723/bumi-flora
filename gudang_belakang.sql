d0fa116c6b24b448c62dcc7507022ee48e25fd80
Bayu <150907897+bayu99723@users.noreply.github.com>
2024-11-26 10:42:12 +0700

    Add files via upload

--- gudang_belakang.sql
+++ gudang_belakang.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 25 Nov 2024 pada 09.08
+-- Waktu pembuatan: 26 Nov 2024 pada 04.39
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.0.30
 
@@ -31,41 +31,21 @@ CREATE TABLE `barang` (
   `id` int(11) NOT NULL,
   `kode_barang` varchar(10) NOT NULL,
   `nama_barang` varchar(100) NOT NULL,
-  `kategori` varchar(50) DEFAULT NULL,
-  `lokasi` varchar(100) DEFAULT NULL
+  `jumlah` int(11) NOT NULL,
+  `unit` enum('buah','kg','liter') NOT NULL,
+  `kategori` varchar(50) NOT NULL,
+  `periode` date DEFAULT NULL,
+  `keterangan` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `barang`
 --
 
-INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `kategori`, `lokasi`) VALUES
-(2, 'BRG001', 'Karet', 'karet', 'medan'),
-(3, 'BRG002', 'buku', 'barang', 'medan');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `nilai_barang`
---
-
-CREATE TABLE `nilai_barang` (
-  `id` int(11) NOT NULL,
-  `barang_id` int(11) NOT NULL,
-  `jumlah` int(11) NOT NULL,
-  `unit` enum('buah','kg','liter') NOT NULL,
-  `periode` date NOT NULL,
-  `keterangan` text DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data untuk tabel `nilai_barang`
---
-
-INSERT INTO `nilai_barang` (`id`, `barang_id`, `jumlah`, `unit`, `periode`, `keterangan`, `created_at`) VALUES
-(1, 2, 2000, 'kg', '2024-11-25', 'masuk hari ini', '2024-11-25 08:00:27'),
-(2, 3, 10, 'buah', '2024-11-25', 'telat 1 jam', '2024-11-25 08:00:27');
+INSERT INTO `barang` (`id`, `kode_barang`, `nama_barang`, `jumlah`, `unit`, `kategori`, `periode`, `keterangan`) VALUES
+(1, 'BRG456', 'pupuk', 40, 'kg', 'pupuk1', NULL, 'Barang rusak'),
+(2, 'BRG002', 'Kunci Busi Shinso', 5, 'buah', 'Alat', NULL, 'Barang rusak'),
+(3, 'BRG003', 'tanah', 20, 'buah', 'pupuk', '2024-11-27', 'Barang OK');
 
 --
 -- Indexes for dumped tables
@@ -79,13 +59,6 @@ ALTER TABLE `barang`
   ADD UNIQUE KEY `kode_barang` (`kode_barang`);
 
 --
--- Indeks untuk tabel `nilai_barang`
---
-ALTER TABLE `nilai_barang`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `barang_id` (`barang_id`);
-
---
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
@@ -94,22 +67,6 @@ ALTER TABLE `nilai_barang`
 --
 ALTER TABLE `barang`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT untuk tabel `nilai_barang`
---
-ALTER TABLE `nilai_barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
---
-
---
--- Ketidakleluasaan untuk tabel `nilai_barang`
---
-ALTER TABLE `nilai_barang`
-  ADD CONSTRAINT `nilai_barang_ibfk_1` FOREIGN KEY (`barang_id`) REFERENCES `barang` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
